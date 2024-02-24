9cfe6b75ba110d2f0f53a9b827e7fa7bb931e427
MushtaqSaeed <mushtaqsaeed460@gmail.com>
2024-02-24 15:12:46 +0500

    New Project

---
+++ If and Else project/app.js
@@ -0,0 +1,87 @@
+//Q1: Get user to input a number and check number is a multiple of 6?
+
+// let number = +prompt("Enter A Number");
+// if( number%6 === 0 ){
+//     document.write ("Your Number Is A Multiple Of 6")}
+// else{
+//     document.write ("Your Number Is Not A Multiple Of 6")
+// }
+
+// ==================================================================================
+
+
+
+
+
+//Q2: Write a code which can give grades to student according to their scores?
+
+// let name1 = prompt("Enter Your Name")
+// let percentage = +prompt("Enter Your Percentage");
+// if( percentage >=90 && percentage <=100){
+//     document.write("Hi " + name1 + ", Your Grade Is A+")
+// }
+// else if( percentage >=80 && percentage <=89){
+//     document.write("Hi " + name1 + ", Your Grade Is A")
+// }
+// else if( percentage >=70 && percentage <=79){
+//     document.write("Hi " + name1 + ", Your Grade Is B")
+// }
+// else if( percentage >=60 && percentage <=69){
+//     document.write("Hi " + name1 + ", Your Grade Is C")
+// }
+// else if( percentage >=50 && percentage <=59){
+//     document.write("Hi " + name1 + ", Your Grade Is D")
+// }
+// else if( percentage >=40 && percentage <=49){
+//     document.write("Hi " + name1 + ", Your Grade Is E")
+// }
+// else if( percentage ==0 && percentage <=39){
+//     document.write("Hi " + name1 + ", Your Grade Is F")
+// }
+// else{
+//     document.write("Invalid Number")
+// }
+
+// ==================================================================================
+
+
+
+
+
+//Q3: Create marksheet using if else and else if condition?
+
+// let name1 = prompt("Enter Your Name");
+// let eng = +prompt("Enter Your English Marks");
+// let urdu = +prompt("Enter Your Urdu Marks");
+// let math = +prompt("Enter Your Mathematics Marks");
+// let isl = +prompt("Enter Your Islamiat Marks");
+// let sci = +prompt("Enter Your Science Marks");
+// let total = (eng + urdu + math + isl + sci); 
+// let percentage = ( total / 500 ) * 100; 
+
+// if( percentage >=90 && percentage <=100){
+//     document.write("Hi " + name1 + ", Your Grade Is A+ And Your Percentage Is " + percentage)
+// }
+// else if( percentage >=80 && percentage <=89){
+//     document.write("Hi " + name1 + ", Your Grade Is A And Your Percentage Is " + percentage)
+// }
+// else if( percentage >=70 && percentage <=79){
+//     document.write("Hi " + name1 + ", Your Grade Is B And Your Percentage Is " + percentage)
+// }
+// else if( percentage >=60 && percentage <=69){
+//     document.write("Hi " + name1 + ", Your Grade Is C And Your Percentage Is " + percentage)
+// }
+// else if( percentage >=50 && percentage <=59){
+//     document.write("Hi " + name1 + ", Your Grade Is D And Your Percentage Is " + percentage)
+// }
+// else if( percentage >=40 && percentage <=49){
+//     document.write("Hi " + name1 + ", Your Grade Is E And Your Percentage Is " + percentage)
+// }
+// else if( percentage >=0 && percentage <=39){
+//     document.write("Hi " + name1 + ", Your Are Fail And Your Percentage Is " + percentage)
+// }
+// else{
+//     document.write("Invalid Number")
+// };
+
+// ===========================================================================================
