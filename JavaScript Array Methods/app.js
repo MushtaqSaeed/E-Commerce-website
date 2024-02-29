80fa9774270de25197bb2fb2595a0006aa8b8108
MushtaqSaeed <mushtaqsaeed460@gmail.com>
2024-02-29 18:43:47 +0500

    javascript array methods

---
+++ JavaScript Array Methods/app.js
@@ -0,0 +1,127 @@
+
+
+
+
+
+
+
+
+// let tablevalue = +prompt ("Enter The Table Value")
+// let tablestartvalue = +prompt ("Enter The Starting Value");
+// let tableendvalue = +prompt ("Enter The End Value");
+
+// for ( let i = tablestartvalue; i <= tableendvalue; i++){
+// document.write( tablevalue + " x " + i + " = " + tablevalue*i + "<br>" )
+// }
+
+// --------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+// let list = ["mushtaq", "mudassir", "ali",4,34,65,8, true, false];
+// let new_list = list.indexOf("mudassir");
+
+// --------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+// let list = ["mushtaq", "mudassir", "ali",4,34,65,8, true, false];
+// let list1 = [ 4,34,65,8, "mushtaq", "mudassir", "ali", true, false];
+// let new_list = list.concat(list1);
+
+// --------------------------------------------------------------------------
+
+
+
+
+
+
+
+// let list = ["mushtaq", "mudassir", "ali", 4, 34, 65, 8, true, false];
+// list.push("Hi");
+
+// --------------------------------------------------------------------------
+
+
+
+
+
+
+// let list = ["mushtaq", "mudassir", "ali",4,34,65,8, true, false];
+// list.pop();
+
+// --------------------------------------------------------------------------
+
+
+
+
+
+
+// let list = ["mushtaq", "mudassir", "ali",4,34,65,8, true, false];
+// list.shift();
+
+// --------------------------------------------------------------------------
+
+
+
+
+
+
+// let list = ["mushtaq", "mudassir", "ali",4,34,65,8, true, false];
+// list.unshift("hamza");
+
+// --------------------------------------------------------------------------
+
+
+
+
+
+
+// let list = ["mushtaq", "mudassir", "ali",4,34,65,8, true, false];
+// let new_list = list.sort();
+
+// --------------------------------------------------------------------------
+
+
+
+
+
+// let list = ["mushtaq", "mudassir", "ali",4,34,65,8, true, false];
+// let new_list = list.slice( 1, 4 );
+
+// --------------------------------------------------------------------------
+
+
+
+
+// let list = ["mushtaq", "mudassir", "ali",4,34,65,8, true, false];
+// let new_list = list.splice( 2, 4 )
+
+// --------------------------------------------------------------------------
+
+
+
+
+// let list = ["mushtaq", "mudassir", "ali",4,34,65,8, true, false];
+// let new_list = list.includes("mudassir");
+
+// --------------------------------------------------------------------------
+
+
+
+
+// document.write(new_list)
