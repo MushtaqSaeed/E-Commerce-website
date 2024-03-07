e379d2172ec0cb4040eca9128f82a4cfecf77eab
MushtaqSaeed <mushtaqsaeed460@gmail.com>
2024-03-07 17:18:36 +0500

    javascript project 2

---
+++ new javascript/app.js
@@ -0,0 +1,45 @@
+
+// let capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let small = "abcdefghijklmnopqrstuvwxyz";
+// let num = "1234567890";
+// let character = "!@#$%^&*()";
+
+// let randomcapital = "";
+// let randomsmall = "";
+// let randomnum = "";
+// let randomcharacter = "";
+
+// for ( let i = 0; i < lenghtofpass/4; i++ ){
+//     random = Math.floor( Math.random() * capital.length )
+//     randomcapital += capital[random] 
+
+//     random1 = Math.floor( Math.random() * small.length )
+//     randomsmall += small[random1] 
+
+//     random2 = Math.floor( Math.random() * num.length )
+//     randomnum += num[random2] 
+
+//     random3 = Math.floor( Math.random() * character.length )
+//     randomcharacter += character[random3] 
+// }
+// let password = (randomcapital + randomsmall + randomnum + randomcharacter)
+// console.log(password)
+
+//------------------------------------------------------------------------------
+
+
+
+// let player1 = prompt("Enter First player Name")
+// let player2 = prompt("Enter Second player Name")
+
+// let random = Math.round ( Math.random() * 2 )
+
+// if ( random == 1 ){
+//     document.write( "Congratulations! " + player1 + " you won the toss" )
+// }
+// else{
+//     document.write( "Congratulations! " + player2 + " you won the toss" )
+// }
+
+//-----------------------------------------------------------------------------
+
